fbe4f9f9b45b2627a60e4a1fd012d115e3829931
Christopher Smith <csmith@whiskey.labs.squiz.net.au>
2012-02-13 10:34:00 +1100

    move the wget/curl check to the top so if they dont exist we dont lose the existing .vim directory

--- vim-profile/setup.sh
+++ vim-profile/setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+curl=`which curl 2>/dev/null`
+if [ $? -gt 0 ]; then
+    wget=`which wget 2>/dev/null`
+    if [ $? -gt 0 ]; then
+        echo "need access to curl or wget."
+        exit
+    fi
+    fetchCmd='wget -O '
+else
+    fetchCmd='curl -so '
+fi
+
 dirs='.vim/autoload .vim/bundle'
 for dir in $dirs; do
     if [ -d ~/${dir} ]; then
@@ -14,19 +26,7 @@ fi
 
 cp -f ./vimrc ~/.vimrc
 
-curl=`which curl 2>/dev/null`
-if [ $? -gt 0 ]; then
-    wget=`which wget 2>/dev/null`
-    if [ $? -gt 0 ]; then
-        echo "need access to curl or wget."
-        exit
-    fi
-    cmd='wget -O '
-else
-    cmd='curl -so '
-fi
-
-${cmd} ~/.vim/autoload/pathogen.vim \
+${fetchCmd} ~/.vim/autoload/pathogen.vim \
   https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
 
 git clone git://github.com/majutsushi/tagbar.git ~/.vim/bundle/tagbar
